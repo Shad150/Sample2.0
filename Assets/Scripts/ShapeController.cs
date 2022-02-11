7b25c32867d227affb2fcdb278a66b9ae29ec754
POG POG PGO GPO GPO POG POG POGPO <cgpablo150@gmail.com>
2022-02-11 13:03:37 +0100

    Rotacion funcional, cubo implementado
    Rotación en los 3 ejes con interpolación (agradecer infinitamente a Torcu, luego le compramos unas pipas)
    Cubo implementado

--- Assets/Scripts/ShapeController.cs
+++ Assets/Scripts/ShapeController.cs
@@ -4,82 +4,113 @@ using UnityEngine;
 
 public class ShapeController : MonoBehaviour
 {
-    bool controlActive=true;
+    private bool controlActive=true, reversed;
     public GameObject parent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform targetDir;
+    private Vector3 targetDegrees;
+    [SerializeField] private float duration = 0.5f;
 
-    // Update is called once per frame
+
     void Update()
     {
         if (controlActive)
         {
+            //print("b");
             if (Input.GetAxis("Horizontal") > 0)
             {
-                StartCoroutine(GiraDer());
-                
+                //StartCoroutine(Gira(parent.transform.forward * 45f));
+                controlActive = false;
+                targetDegrees = Vector3.up * -90f;
+                targetDir.Rotate(targetDegrees, Space.World);
+                StartCoroutine(Gira());
             }
             else if (Input.GetAxis("Horizontal") < 0)
             {
-                StartCoroutine("GiraIzq");
+                //StartCoroutine(Gira(parent.transform.forward * -45f));
+                controlActive = false;
+                targetDegrees = Vector3.up * 90f;
+                targetDir.Rotate(targetDegrees, Space.World);
+                StartCoroutine(Gira());
+            }
+            else if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(1))
+            {
+                //StartCoroutine(Gira(parent.transform.forward * 45f));
+                controlActive = false;
+                targetDegrees = Vector3.forward * -90f;
+                targetDir.Rotate(targetDegrees, Space.World);
+                StartCoroutine(Gira());
+            }
+            else if (Input.GetKeyDown(KeyCode.Q) || Input.GetMouseButtonDown(0))
+            {
+                //StartCoroutine(Gira(parent.transform.forward * -45f));
+                controlActive = false;
+                targetDegrees = Vector3.forward * 90f;
+                targetDir.Rotate(targetDegrees, Space.World);
+                StartCoroutine(Gira());
             }
             else if (Input.GetAxis("Vertical") > 0)
             {
-                StartCoroutine("GiraBot");
+                //StartCoroutine(Gira(parent.transform.right * 45f));
+                controlActive = false;
+                targetDegrees = Vector3.right * 90f;
+                targetDir.Rotate(targetDegrees, Space.World);
+                StartCoroutine(Gira());
             }
             else if (Input.GetAxis("Vertical") < 0)
             {
-                StartCoroutine("GiraBot");
+                //StartCoroutine(Gira(parent.transform.right * -45f));
+                controlActive = false;
+                targetDegrees = Vector3.right * -90f;
+                targetDir.Rotate(targetDegrees, Space.World);
+                StartCoroutine(Gira());
             }
         }
-        
-    }
-
-    IEnumerator GiraDer() {
-        controlActive = false;
-        float cont = 0;
-        //Quaternion initial= parent.transform.rotation;
-        //Quaternion final = parent.transform.rotation * Quaternion.Euler(0,90,0); ;
-        //parent.transform.Rotate(0, 90, 0);
-        parent.transform.RotateAround(parent.transform.position, Vector3.up, 90);
-        //while (cont<1)
+        //else
         //{
-        //    parent.transform.rotation = Quaternion.Lerp(initial, final, cont);
-        //    cont += Time.deltaTime*0.01f;
-        //    Debug.Log(cont);
+        //    //print((parent.transform.rotation * Quaternion.Inverse(targetDir.rotation)).eulerAngles.magnitude % 359);
+        //    float diff = Vector3.Distance(parent.transform.rotation.eulerAngles, targetDir.rotation.eulerAngles);
+        //    print(diff%90);
+        //    if (reversed)
+        //    {
+        //        if (diff % 90 < 5)
+        //        {
+        //            print("a");
+        //            parent.transform.rotation = targetDir.rotation;
+        //            controlActive = true;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (diff % 90 -90f < 5)
+        //        {
+        //            print("a");
+        //            parent.transform.rotation = targetDir.rotation;
+        //            controlActive = true;
+        //        }
+        //    }
+
+
+        //    parent.transform.Rotate(targetDegrees * Time.deltaTime, Space.World);
+
         //}
 
-        yield return new WaitForSeconds(.3f);
-        controlActive = true;
-    }
 
-    IEnumerator GiraIzq()
-    {
-        controlActive = false;
-        //parent.transform.Rotate(0, -90, 0);
-        parent.transform.RotateAround(parent.transform.position, Vector3.up, -90);
-        yield return new WaitForSeconds(.3f);
-        controlActive = true;
-    }
+        IEnumerator Gira()
+        {
+            controlActive = false;
+            Quaternion initial = parent.transform.rotation;
 
-    IEnumerator GiraTop()
-    {
-        controlActive = false;
-        parent.transform.RotateAround(parent.transform.position, Vector3.right, 90);
-        yield return new WaitForSeconds(.3f);
-        parent.transform.Rotate(0, 0, 90);
-        controlActive = true;
-    }
-
-    IEnumerator GiraBot()
-    {
-        controlActive = false;
-        parent.transform.RotateAround(parent.transform.position, Vector3.right, -90);
-        yield return new WaitForSeconds(.3f);
-        //parent.transform.Rotate(0, 0, -90);
-        controlActive = true;
+            for (float i = 0; i < duration; i += Time.deltaTime)
+            {
+                parent.transform.rotation = Quaternion.Slerp(initial, targetDir.rotation, i / duration);
+                yield return null;
+            }
+            parent.transform.rotation = targetDir.rotation;
+            //parent.transform.Rotate(0, -90, 0);
+            //parent.transform.RotateAround(parent.transform.position, Vector3.up, -90);
+            //parent.transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * Quaternion.Euler(0, -90, 0), 0.3f);
+            yield return new WaitForSeconds(.2f);
+            controlActive = true;
+        }
     }
 }
